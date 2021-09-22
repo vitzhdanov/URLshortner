985b9b1f10d0682a9e013abd12c491bf8bebd4e0
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-09-22 11:05:36 +0300

    Diploy

--- URLshortner/settings.py
+++ URLshortner/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'URLshortner.urls'
@@ -78,8 +79,19 @@ WSGI_APPLICATION = 'URLshortner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'd4i63va13c2h5m',
+
+        'HOST': 'ec2-44-194-6-121.compute-1.amazonaws.com',
+
+        'PORT': 5432,
+
+        'USER': 'amakcmozipfxtk',
+
+        'PASSWORD': '8b7919d7fbde9d56cee768733d1c3154ee2d8bac6f580392ee5de5d98596df7b',
+
     }
 }
 
@@ -129,3 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+db_from_env = dj_database_url.config()
