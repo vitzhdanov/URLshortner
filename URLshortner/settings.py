43e40458855f00964f334786bd3245661f65e6f1
vitzhdanov <vladislav.vitzhdanov@gmail.com>
2021-09-22 10:13:41 +0300

    Diploy

--- URLshortner/settings.py
+++ URLshortner/settings.py
@@ -79,8 +79,19 @@ WSGI_APPLICATION = 'URLshortner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'd5v3d2atr7p0m8',
+
+        'HOST': 'ec2-44-194-6-121.compute-1.amazonaws.com',
+
+        'PORT': 5432,
+
+        'USER': 'amakcmozipfxtk',
+
+        'PASSWORD': '8b7919d7fbde9d56cee768733d1c3154ee2d8bac6f580392ee5de5d98596df7b',
+
     }
 }
 
